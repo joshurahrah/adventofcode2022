a9960a3fe556b3a1d544251d935d737fc9f364ca
Joshua Whittle <joshurahrah@outlook.com>
2022-12-01 13:08:20 +0000

    day 1 puzzle

---
+++ Day1/Program.cs
@@ -0,0 +1,35 @@
+﻿await Run();
+
+static async Task Run()
+{
+    var elfCalories = await ReadElfCaloriesAndSum("./PuzzleInput.txt");
+
+    Console.WriteLine(elfCalories.Max());
+
+    Console.WriteLine(elfCalories.OrderByDescending(x => x).Take(3).Sum());
+}
+
+
+static async Task<List<int>> ReadElfCaloriesAndSum(string path)
+{
+    using var streamReader = new StreamReader(path);
+
+    int currentSum = 0;
+    List<int> elfCalories = new();
+    while(!streamReader.EndOfStream)
+    {
+        var line = await streamReader.ReadLineAsync();
+        int value = 0;
+        if(string.IsNullOrWhiteSpace(line))
+        {
+            elfCalories.Add(currentSum);
+            currentSum = 0;
+        }
+        else if(int.TryParse(line, out value))
+        {
+            currentSum += value;
+        }
+    }
+
+    return elfCalories;
+}
