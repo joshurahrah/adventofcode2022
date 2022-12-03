527d58643ce9c141b9f753650ab5beb961f5a1ba
joshua.whittle@itv.com <joshwhi1@f222yl7mxq.home>
2022-12-03 09:35:07 +0000

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,40 @@
+﻿await Run();
+
+static async Task Run()
+{
+    using var streamReader = new StreamReader("./PuzzleInput.txt");
+
+    int totalScore = 0;
+    List<string> rucksacks = new List<string>();
+    while (!streamReader.EndOfStream)
+    {
+        rucksacks.Add(await streamReader.ReadLineAsync());
+    }
+
+    for(int i = 0; i < rucksacks.Count; i = i + 3)
+    {
+        var firstElf = rucksacks[i];
+        var secondElf = rucksacks[i + 1];
+        var thirdElf = rucksacks[i + 2];
+
+        var common = firstElf.Join(secondElf,
+                                    f => f,
+                                    s => s,
+                                    (f, s) => f)
+                                .Join(thirdElf,
+                                    f => f,
+                                    t => t,
+                                    (f, t) => f)
+                                    .Distinct()
+                                    .First();
+        
+
+        var numeric = Char.IsUpper(common)
+                        ? (int)Convert.ToChar(common) - 38
+                        : (int)Convert.ToChar(common) - 96;
+    
+        totalScore += numeric;
+    }
+
+    Console.WriteLine(totalScore);
+}
