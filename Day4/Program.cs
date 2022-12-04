5e61a8f91ebaabff8a7a3e9c9d4b80e284769d73
joshua.whittle@itv.com <joshwhi1@f222yl7mxq.home>
2022-12-04 08:02:48 +0000

    Day 4 Part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,29 @@
+﻿await Run();
+
+static async Task Run()
+{
+    using var streamReader = new StreamReader("./PuzzleInput.txt");
+
+    int fullyContained = 0;
+    while (!streamReader.EndOfStream)
+    {
+        var line = await streamReader.ReadLineAsync();
+
+        var sections = line.Split(',');
+        
+        var elfOne = sections[0].Split('-').Select(x => int.Parse(x)).ToArray();
+        var elfTwo = sections[1].Split('-').Select(x => int.Parse(x)).ToArray();
+
+        if((elfOne[0] <= elfTwo[0]
+            && elfOne[1] >= elfTwo[1])
+            || (elfTwo[0] <= elfOne[0]
+                && elfTwo[1] >= elfOne[1]))
+            {
+                fullyContained += 1;
+            }
+
+        
+    }
+
+    Console.WriteLine(fullyContained);
+}
